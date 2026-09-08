dde323e89c36772dc6f90cf2a45d658347f0315f
Crabbercat <102230292@sv1.dut.udn.vn>
2026-09-09 00:54:38 +0700

    first set up

---
+++ README.md
@@ -0,0 +1,22 @@
+# IoT Labs
+
+Personal IoT laboratory workspace on Raspberry Pi.
+
+## Hardware
+
+- Raspberry Pi 4
+- ESP32
+- Arduino
+- DHT11 / DHT22
+- LEDs
+- Buttons
+- Other sensors
+
+## Labs
+
+| Lab | Description |
+|---|---|
+| 01 | LED Blink |
+| 02 | Button |
+| 03 | DHT Temperature/Humidity |
+
---
+++ labs/01_blink/main.py
@@ -0,0 +1,25 @@
+import lgpio
+import time
+
+GPIO_PIN = 17
+
+h = lgpio.gpiochip_open(0)
+
+try:
+    lgpio.gpio_claim_output(h, GPIO_PIN, 0)
+
+    while True:
+        lgpio.gpio_write(h, GPIO_PIN, 1)
+        print("LED ON")
+        time.sleep(1)
+
+        lgpio.gpio_write(h, GPIO_PIN, 0)
+        print("LED OFF")
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    print("\nStopping...")
+
+finally:
+    lgpio.gpio_free(h, GPIO_PIN)
+    lgpio.gpiochip_close(h)
